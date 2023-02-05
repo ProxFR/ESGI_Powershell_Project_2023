3ca29c74017c97798664c050a50ddbe1c49c44df
Nirvangi <101008244+Nirvangi@users.noreply.github.com>
2023-02-05 18:28:02 +0100

    Add files via upload
    Ajout de fonctionnalité de création de VM

--- main.ps1
+++ main.ps1
@@ -3,23 +3,23 @@ Connect-AzAccount
 
 function creationVM {
 
-    param(
-        [String] $VMName
+# Vérification de l'existance de la "ressource group"
+$resourceGroup = Get-AzResourceGroup
+    if ($resourceGroup.ResourceGroupName -eq "VM-Projet-Powershell"){
+        #Récupérer les informations d'authentification pour la connexion à la machine
 
-    )
+        $ComputerName = read-host "Entrez le nom de la VM"
+        $Location = "North Europe"
+        $ServiceName ="MonCloudapp"
+        $InstanceSize="A6"
+        $ImageName="fb83b3509582419d99629ce476bcb5c8__SQL-Server-2014RTM-12.0.2000.8-Standard-ENU-WS2012R2-AprilGA"
+        $NumberOfDisks=8
+        $DiskSize=200
+        $SubnetName="Subnet-1"
+        $MediaLocation="https://XXXXXXX.blob.core.windows.net/vhds"
+        #$credential = Get-Credential
 
-    #Récupérer les informations d'authentification pour la connexion à la machine
-    $Location = "North Europe"
-    $ComputerName = "MonSqlServer"
-    $ServiceName ="MonCloudapp"
-    $InstanceSize="A6"
-    $ImageName="fb83b3509582419d99629ce476bcb5c8__SQL-Server-2014RTM-12.0.2000.8-Standard-ENU-WS2012R2-AprilGA"
-    $NumberOfDisks=8
-    $DiskSize=200
-    $SubnetName="Subnet-1"
-    $MediaLocation="https://XXXXXXX.blob.core.windows.net/vhds"
-    $credential = Get-Credential
-
+    }
 }
 
 function ListVM{
@@ -41,12 +41,13 @@ function InstalServiceVM {
 
 function main {
 
-    $rep = read-host "Que voulez-vous faire?/n
+    $rep = read-host "Que voulez-vous faire?
     1. Création d'une VM
     2. Lister les VM existantes
     3. Supprimer une VM
     4. Arreter ou démarrer une VM
-    5. Installation de service"
+    5. Installation de service
+    "
     
     switch ($rep){
         { $_ -eq 1 } { creationVM }
@@ -54,6 +55,9 @@ function main {
         { $_ -eq 3 } { SupprimerVM }
         { $_ -eq 4 } { GestionVM }
         { $_ -eq 5 } { InstalServiceVM }
+        Default {}
     }
 
 }
+
+main
