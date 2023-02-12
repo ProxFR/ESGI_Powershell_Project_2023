e313f5ce57d4000ee7defbacbcbf512e7a0c1a69
ProxFR <tristan.cacchia@outlook.fr>
2023-02-12 12:58:12 +0100

    Measuring

--- BenckmarkingTool_Thread.ps1
+++ BenckmarkingTool_Thread.ps1
@@ -1,4 +1,4 @@
-$Scriptblock = {
+$Scriptblock = { # Utilisation de la formule de Leibniz
     $k = 0
     $pi = 0
     $limit = 10000000 + 5
@@ -11,18 +11,21 @@ $Scriptblock = {
     } 
 }
 
-$MaxThreads = 1
-$RunspacePool = [runspacefactory]::CreateRunspacePool(1, $MaxThreads)
-$RunspacePool.Open()
-$Jobs = @()
-
-1..10 | Foreach-Object {
-	$PowerShell = [powershell]::Create()
-	$PowerShell.RunspacePool = $RunspacePool
-	$PowerShell.AddScript($ScriptBlock).AddArgument($_)
-	$Jobs += $PowerShell.BeginInvoke()
-}
-
-while ($Jobs.IsCompleted -contains $false) {
-	Start-Sleep 1
-}
+Measure-Command {
+    $MaxThreads = 1 # Nombre de threads physiques
+    $RunspacePool = [runspacefactory]::CreateRunspacePool(1, $MaxThreads)
+    $RunspacePool.Open()
+    $Jobs = @()
+    
+    1..1 | Foreach-Object { # Nombre de threads logiques
+        Write-Host "Lancement du thread $_" -ForegroundColor Green
+        $PowerShell = [powershell]::Create()
+        $PowerShell.RunspacePool = $RunspacePool
+        $PowerShell.AddScript($ScriptBlock).AddArgument($_)
+        $Jobs += $PowerShell.BeginInvoke()
+    }
+    
+    while ($Jobs.IsCompleted -contains $false) {
+        Start-Sleep -Milliseconds 100
+    }
+} | Select-Object TotalSeconds
