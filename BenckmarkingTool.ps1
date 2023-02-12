6b7b5f8fd6c4cf401db9fb9394f9b5d09d9472ce
ProxFR <tristan.cacchia@outlook.fr>
2023-02-12 10:37:08 +0100

    Cleaning

--- BenckmarkingTool.ps1
+++ BenckmarkingTool.ps1
@@ -1,19 +1,18 @@
 $decimals = Read-Host "Combien de décimales de Pi souhaitez-vous calculer?" #1000000
-$startTime = Get-Date
 
 $k = 0
 $pi = 0
 $limit = $decimals + 5
 $sign = 1
 
-while ($k -lt $limit) {
-  $pi = $pi + $sign * 4 / (2 * $k + 1)
-  $k++
-  $sign *= -1
+$executionTime = Measure-Command {
+  while ($k -lt $limit) {
+    $pi = $pi + $sign * 4 / (2 * $k + 1)
+    $k++
+    $sign *= -1
+  }
 }
 
 $pi = "{0:N$decimals}" -f $pi
-Write-Host $pi
-
-$elapsedTime = (Get-Date) - $startTime
-Write-Host "Elapsed time: $($elapsedTime.TotalSeconds) seconds"
+Write-Host "Calcul de Pi terminé en $($executionTime.TotalSeconds) secondes."
+#Write-Host $pi
