0736df03cd8d2342af64791728369d27fa340361
tcolin98 <123875942+tcolin98@users.noreply.github.com>
2023-02-16 18:23:15 +0100

    Update Install grafana-agent V1.ps1

--- Install grafana-agent V1.ps1
+++ Install grafana-agent V1.ps1
@@ -12,7 +12,7 @@ foreach ($singleVM in $vm) {
   Write-Host "Installation de l'agent Grafana sur $($singleVM.Name)..."
   $result = Invoke-Command -ComputerName $singleVM.Name -ScriptBlock {
     # Code pour installer Grafana-Agent sur la machine virtuelle actuelle
-    curl -L https://raw.githubusercontent.com/grafana/agent/release/production/grafanacloud-install.ps1 --output grafanacloud-install.ps1
+    wget https://raw.githubusercontent.com/grafana/agent/release/production/grafanacloud-install.ps1 -outfile grafanacloud-install.ps1
     powershell -executionpolicy Bypass -File ".\grafanacloud-install.ps1" -GCLOUD_STACK_ID "532357" -GCLOUD_API_KEY "eyJrIjoiNmJlOGI5ZmU0ZmU0YjRhODgxMDgwZjE5YzdhMGY4OWQ1M2Q2YzZiZiIsIm4iOiJzdGFjay01MzIzNTctZWFzeXN0YXJ0LWdjb20iLCJpZCI6NzkxMzEzfQ==" -GCLOUD_API_URL "https://integrations-api-eu-west.grafana.net"
     # Retourne un résultat indiquant si l'installation a réussi ou non
     return $true
