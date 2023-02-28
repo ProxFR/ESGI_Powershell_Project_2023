0716bc21fbf21c7a92c7f8d8b52458dfa99d59bf
ProxFR <tristan.cacchia@outlook.fr>
2023-02-28 14:14:47 +0100

    update

--- Test_WinRM.ps1
+++ Test_WinRM.ps1
@@ -8,7 +8,7 @@ function pingTest {
 $username = Read-Host "Enter username"
 $pass = Read-Host "Enter password" -AsSecureString 
 $cred = New-Object -typename System.Management.Automation.PSCredential -argumentlist $username, $pass
-$s = New-PSSession -ConnectionUri 'http://23.102.34.8:5985' -Credential $cred -SessionOption (New-PSSessionOption -SkipCACheck -SkipCNCheck -SkipRevocationCheck)
-#Enter-PSSession -ConnectionUri 'http://137.116.250.105:5985' -Credential $cred
+#$s = New-PSSession -ConnectionUri 'http://23.102.34.8:5985' -Credential $cred -SessionOption (New-PSSessionOption -SkipCACheck -SkipCNCheck -SkipRevocationCheck)
+Enter-PSSession -ConnectionUri 'http://23.102.34.8:5985' -Credential $cred
 
-Invoke-Command -Session $s -ScriptBlock ${function:pingTest}
+#Invoke-Command -Session $s -ScriptBlock ${function:pingTest}
