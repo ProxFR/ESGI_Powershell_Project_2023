886d4e9ad50b57606c6d23696e90a9652ba47195
ProxFR <tristan.cacchia@outlook.fr>
2023-03-01 14:34:07 +0100

    Upate path in agent-config.yaml

--- Test_WinRM.ps1
+++ Test_WinRM.ps1
@@ -9,6 +9,6 @@ $username = Read-Host "Enter username"
 $pass = Read-Host "Enter password" -AsSecureString 
 $cred = New-Object -typename System.Management.Automation.PSCredential -argumentlist $username, $pass
 #$s = New-PSSession -ConnectionUri 'http://20.223.247.119:5985' -Credential $cred -SessionOption (New-PSSessionOption -SkipCACheck -SkipCNCheck -SkipRevocationCheck)
-Enter-PSSession -ConnectionUri 'http://20.166.4.8:5985' -Credential $cred
+Enter-PSSession -ConnectionUri 'http://20.123.14.184:5985' -Credential $cred
 
 #Invoke-Command -Session $s -ScriptBlock ${function:pingTest}
